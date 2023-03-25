84ff0ee405becfbb88f2ae1a17cbd1e95aa5d66f
GerardCod <gerardo.aguilar.calderon@gmail.com>
2023-03-25 16:36:13 -0600

    add: Agregando las primeras clases del modelo

---
+++ src/main/kotlin/com/cafeteria/bedu/model/Product.kt
@@ -0,0 +1,10 @@
+package com.cafeteria.bedu.model
+
+/**
+ * Clase que representa un producto de la cafetería.
+ */
+data class Product(
+    var name: String,
+    var price: Float,
+    var flavor: String
+)
---
+++ src/main/kotlin/com/cafeteria/bedu/model/Size.kt
@@ -0,0 +1,11 @@
+package com.cafeteria.bedu.model
+
+/**
+ * Enum con los valores de los tamaños de los diferentes productos de la cafetería.
+ */
+enum class Size {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    EXTRA_LARGE
+}
