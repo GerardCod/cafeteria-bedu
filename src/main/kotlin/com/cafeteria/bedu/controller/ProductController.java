9dc8d16a17978e9c4579bedb383886a829ee1fb0
GerardCod <gerardo.aguilar.calderon@gmail.com>
2023-03-26 09:25:51 -0600

    edit: Terminando la interfaz ProductController

--- src/main/kotlin/com/cafeteria/bedu/controller/ProductController.java
+++ src/main/kotlin/com/cafeteria/bedu/controller/ProductController.java
@@ -1,2 +1,12 @@
-package com.cafeteria.bedu.controller;public interface ProductController {
+package com.cafeteria.bedu.controller;
+
+import com.cafeteria.bedu.model.entities.Discount;
+import com.cafeteria.bedu.model.entities.Order;
+import com.cafeteria.bedu.model.entities.Product;
+
+public interface ProductController {
+    Order showOrder();
+    void addProductToOrder(Product product);
+    void removeProductFromOrder(Product product);
+    Float payOrder(Discount discount, Float cash);
 }
